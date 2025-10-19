9be118e3864c3abffbaf347e09de9aac016f9da8
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-10-18 22:33:04 -0400

    Primeiro 'Olá mundo com o FastAPI'

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+@app.get("/")
+def read_root():
+    return {'message': 'Olá Mundo!'}
+
