4a6ebdab0df7be0ae2bf20c341158da12549e895
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-10-18 22:55:24 -0400

    Instalação e configuração das primeiras ferramentas de desenvolvimento

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
-
-@app.get("/")
-def read_root():
-    return {'message': 'Olá Mundo!'}
-
