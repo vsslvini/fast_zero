994d39a38311123442eb2c840aa13faf10cdf77b
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-10-31 20:18:07 -0400

    Resolução do exercicio - Aula 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_deve_retornar_html_com_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá Mundo!</h1>' in response.text
