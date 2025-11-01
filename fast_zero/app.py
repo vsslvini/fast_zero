104d557514843ff050986e94cd5a8266891e0d01
Vinicius Alencar <vsslviniciussousa@gmail.com>
2025-10-31 20:18:07 -0400

    Resolução do exercicio - Aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-
 from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from schemas import Message
 
@@ -10,3 +11,17 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ola', response_class=HTMLResponse)
+def ola():
+    return """
+    <html>
+        <head>
+            <title>Olá Mundo</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!</h1>
+        </body>
+    </html>
+    """
